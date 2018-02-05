2118a58a03d496894e6b573d76b8278749ad00fc
kantologist <azeezfemi17937@yahoo.com>
2018-02-05 15:00:13 +0100

    created payment form and verified proper angular data binding

--- controllers/payment.js
+++ controllers/payment.js
@@ -2,6 +2,7 @@
 
 exports.getPaymentPage = function(req, res, next) {
     return res.render('payment/payment_view', {
-        title : 'eshuri payment page'
+        title : 'eshuri payment page',
+        csrf_token: res.locals.csrftoken
     })
 }
