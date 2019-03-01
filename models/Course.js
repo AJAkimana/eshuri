6f09865b6cc40449eaa2f7c762a8c08ed8957540
Akimana A Jean dAmour <akimanaja17@gmail.com>
2019-03-01 10:39:12 +0200

    [#164325866] Structure course term

--- models/Course.js
+++ models/Course.js
@@ -10,7 +10,7 @@ var CourseSchema =new Schema({
 	school_id:{type: Schema.Types.ObjectId,required:true,unique:false},
 	class_id:{type: Schema.Types.ObjectId,required:true,unique:false},
 	teacher_list:{type:[Schema.Types.ObjectId],required:true,unique:false},
-	currentTerm:{type:Number,required:true, min:0,unique:false},
+	courseTerm:{type:Number,required:true, min:0,unique:false},
   level:{type:Number,required:true, min:0,unique:false},
   isConsidered:{type:Boolean,required:false, default:true, unique:false},
         /* Useful for the report */
